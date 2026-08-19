ae74824c73e13c13504f16ed30a1002e03d0dab7
DEBANJAN BHAUMIK <94661241+debanjan2606@users.noreply.github.com>
2026-08-19 17:08:44 +0530

    Refactor App component to include Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Education from "./components/Education";
-import Certifications from "./components/Certifications";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Hero />
+
       <About />
-      <Education />
+
+      <Experience />
+
       <Skills />
+
       <Projects />
+
+      <Education />
+
       <Certifications />
+
       <Contact />
+
       <Footer />
-      
     </>
   );
 }
